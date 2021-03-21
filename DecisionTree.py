b90122ad020c4524831d0d34a82f8eed616f1b2b
Marouf Shaikh <maruf98@gmail.com>
2021-03-21 00:27:09 +0000

    prints score and confusion matrix

--- DecisionTree.py
+++ DecisionTree.py
@@ -1,4 +1,7 @@
 from sklearn import tree
+from sklearn.model_selection import train_test_split    # for stratified sampling
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -24,3 +27,16 @@ df = calls_df.join(contract_df.set_index(['Area Code', 'Phone']), on = ['Area Co
 X = df.drop(['Churn', 'Day Charge', 'Night Charge', 'Intl Charge', 'State', 'Area Code', 'Phone'], axis = 1)
 y = df[['Churn']]
 
+# creating a train-test split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 42)
+
+# creating the tree classifier
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(X_train, y_train)
+
+# score of the tree predictor
+print(clf.score(X_test, y_test))
+
+# creating confusion matrix
+y_pred = clf.predict(X_test)
+confusion_matrix(y_test, y_pred)
